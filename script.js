3081475ae3c7dc855741ba9b7c97f60f80edffd9
Ruchita Lodha <ruchita.lodha@zopnow.com>
2017-07-06 23:43:39 +0530

    Added content pages frame.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-$(document).ready(function(){
-	$('#menu-toggle').click(function() {
-	    $(this).toggleClass('active');
-	    $('.logo').toggleClass('logo-menu-open');
-	    $('.menu-overlay').toggleClass('open');
-	  });
+$(document).ready(function () {
+    $('#menu-toggle').click(function () {
+        $(this).toggleClass('active');
+        $('.logo').toggleClass('logo-menu-open');
+        $('.menu-overlay').toggleClass('open');
+    });
 
-	  $('.main-nav li a').click(function() {
-	    $('#menu-toggle').removeClass('active');
-	    $('.logo').removeClass('logo-menu-open');
-	    $('.menu-overlay').removeClass('open');
-	  });
+    $('.main-nav li a').click(function () {
+        $('#menu-toggle').removeClass('active');
+        $('.logo').removeClass('logo-menu-open');
+        $('.menu-overlay').removeClass('open');
+        $("body, html").animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 600);
+    });
 
-	$('a[href="#top"]').click(function() {
-	    $('#menu-toggle').removeClass('active');
-	    $('.logo').removeClass('logo-menu-open');
-    $('.menu-overlay').removeClass('open');
-  });
+    $('a[href="#top"]').click(function () {
+        $('#menu-toggle').removeClass('active');
+        $('.logo').removeClass('logo-menu-open');
+        $('.menu-overlay').removeClass('open');
+    });
+
 });
